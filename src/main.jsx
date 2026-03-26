9fd4d722ac3fe878386e8f539f828a873769a73b
service-vandenbos <service@vandenbos-cm.nl>
2026-03-26 12:12:30 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App(){
+  return <div style={{padding:40}}>VDB Service App werkt 🚀</div>
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
